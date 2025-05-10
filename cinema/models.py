0101f50e806cd976a81d09a9b90a459a7c599b69
Oleksandr <aav162807@gmail.com>
2025-05-10 14:07:33 -0500

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -1,6 +1,10 @@
+import pathlib
+import uuid
+
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 
 
 class CinemaHall(models.Model):
@@ -35,8 +39,9 @@ class Actor(models.Model):
         return f"{self.first_name} {self.last_name}"
 
 
-def bus_image_path(instance: "Movie", filename: str) -> str:
-
+def bus_image_path(instance: "Movie", filename: str) -> pathlib.Path:
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}" + pathlib.Path(filename).suffix
+    return pathlib.Path("upload/movies") / pathlib.Path(filename)
 
 
 class Movie(models.Model):
@@ -45,7 +50,7 @@ class Movie(models.Model):
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
-    image = models.ImageField(null=True, upload_to="media/upload/")
+    image = models.ImageField(null=True, upload_to="bus_image_path")
 
     class Meta:
         ordering = ["title"]
